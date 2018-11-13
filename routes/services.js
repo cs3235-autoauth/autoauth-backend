8fd5fcd74171ad210409e930b1ff8bc19df506d8
Keith Ong <keith.oys@gmail.com>
2018-11-13 20:59:11 +0800

    - Added new schema - Completed registration API

--- routes/services.js
+++ routes/services.js
@@ -3,11 +3,21 @@ const router = express.Router();
 const qp = require('flexqp');
 const rb = require('flexrb');
 
-router.get('/authenticate/:email', async function (req, res, next) {
+router.post('/authenticate', async function (req, res, next) {
     try {
         if (req.err) return next(req.err);
-        let data = await qp.executeAndFetchFirstPromise('SELECT * FROM autoauth WHERE email like ?', [req.params.email]);
-        res.json(rb.build(data, 'Retrieved user successfully.'));
+        let fingerprint_Incoming = req.body.fingerprint;
+        let email = req.body.email;
+        let fingerprint_DB = await qp.executeAndFetchFirstPromise('SELECT fingerprint FROM autoauth WHERE email like ?', [email]);
+
+        if (fingerprint_DB == null) {
+            res.statusCode = 401;
+            res.json('Unauthorized.');
+        } else {
+            console.log(fingerprint_DB);
+            compareJSON(JSON.parse(fingerprint_Incoming), JSON.parse(fingerprint_DB));
+            res.json(rb.build('Retrieved user successfully.'));
+        }
     } catch (err) {
         err.status = 406;
         next(err);
@@ -18,15 +28,20 @@ router.post('/register', async function (req, res, next) {
     let con = await qp.connectWithTbegin();
     try {
         if (req.err) return next(req.err);
-        let data = req.body.fingerprint;
-        await qp.execute('INSERT into autoauth SET fingerprint = ?', [data], con);
+        let fingerprint = JSON.stringify(req.body.fingerprint);
+        let email = req.body.email;
+        await qp.execute('INSERT into autoauth SET fingerprint = ?, email = ?', [fingerprint, email], con);
         await qp.commitAndCloseConnection(con);
-        res.json(rb.build(data, 'Created user successfully.'));
+        res.json(rb.build('Created user successfully.'));
     } catch (err) {
         await qp.rollbackAndCloseConnection(con);
         err.status = 406;
         next(err);
     }
 })
+
+function compareJSON(a, b) {
+    
+}
 
 module.exports = router;
